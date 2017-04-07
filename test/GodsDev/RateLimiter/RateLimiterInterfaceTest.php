33e0cd24ced2a739349ebe4f92f127591151f53a
tomaskraus <tomas.krausik@gmail.com>
2017-04-07 22:47:08 +0200

    rename and update test methods

--- test/GodsDev/RateLimiter/RateLimiterInterfaceTest.php
+++ test/GodsDev/RateLimiter/RateLimiterInterfaceTest.php
@@ -31,10 +31,8 @@ abstract class RateLimiterInterfaceTest extends \PHPUnit_Framework_TestCase
 
 
     protected function setUp() {
-        //echo("==sUp start==");
         $limiter = $this->createRateLimiter();
         $this->limiterWrapper = $this->createLimiterTimeWrapper($limiter);
-        //echo("==sUp end==");
     }
 
     /**
@@ -82,11 +80,13 @@ abstract class RateLimiterInterfaceTest extends \PHPUnit_Framework_TestCase
     }
 
 
-    //-TESTS-------------------------------------------------------------------------
-
-    public function testImplements() {
-        $this->assertInstanceOf('GodsDev\RateLimiter\RateLimiterInterface', $this->getLimiterWrapper()->getLimiter());
+    protected function ensureMaximumHitsIsMade() {
+        $w = $this->getLimiterWrapper();
+        $rate = $w->getLimiter()->getRate();
+        $this->assertEquals($rate, $w->getHits(), '<rate> hits consumed within the period');
     }
+
+    //-TESTS-------------------------------------------------------------------------
 
     public function testMinimalRateAndPeriodValues() {
         $w = $this->getLimiterWrapper();
@@ -96,96 +96,130 @@ abstract class RateLimiterInterfaceTest extends \PHPUnit_Framework_TestCase
     }
 
 
-    public function testInitialLimiterValues() {
+    public function test_Implements_RateLimiterInterface() {
+        $this->assertInstanceOf('GodsDev\RateLimiter\RateLimiterInterface', $this->getLimiterWrapper()->getLimiter());
+    }
+
+    //---------------
+
+    public function test_Zero_Hits_Zero_TimeToWait() {
         $w = $this->getLimiterWrapper();
 
         $this->assertEquals(0, $w->getHits(), 'zero hits after creation');
         $this->assertEquals(0, $w->getTimeToWait(), 'needs no wait after creation');
     }
 
-    public function testResetLimiter() {
+    public function test_Zero_Hits_Zero_TimeToWait_After_Reset() {
         $w = $this->getLimiterWrapper();
 
-        $this->assertTrue($w->inc(), 'can inc at the first time');
-        $this->assertEquals(1, $w->getHits(), 'hits have incremeted after inc');
-
         $w->reset();
-
-        $this->assertEquals(0, $w->getHits(), 'zero hits after a reset');
-        $this->assertEquals(0, $w->getTimeToWait(), 'no need to wait after a reset');
-        $this->assertTrue($w->inc(), 'can inc after a reset');
-        $this->assertEquals(1, $w->getHits(), 'hits have incremeted after inc after reset');
+        $this->test_Zero_Hits_Zero_TimeToWait();
     }
 
-    public function testNoHitsZeroWaitAfterPeriodHasGone() {
+    public function test_Inc_Increments_Hits_By_One() {
+        $w = $this->getLimiterWrapper();
+
+        $currentHits = $w->getHits();
+        $newHits = $w->inc();
+        $this->assertTrue($newHits == $currentHits + 1, 'call of inc() increments hits by one');
+    }
+
+    public function test_Can_Increment_Before_And_After_Reset_In_Period() {
+        $w = $this->getLimiterWrapper();
+
+        $this->test_Inc_Increments_Hits_By_One();
+        $w->reset();
+        $this->test_Zero_Hits_Zero_TimeToWait();
+        $this->test_Inc_Increments_Hits_By_One();
+    }
+
+    public function test_No_Hits_Zero_Wait_After_End_Of_Period() {
         $w = $this->getLimiterWrapper();
         $period = $w->getLimiter()->getPeriod();
 
+        $this->test_Inc_Increments_Hits_By_One();
 
-        $this->assertTrue($w->inc(), 'can inc on a start');
-        $this->assertEquals(1, $w->getHits(), 'hits have incremeted after inc');
-        $w->wait($period + 1, "testNoHitsZeroWaitAfterPeriodHasGone"); //we exceed the limiter's period
-        $this->assertEquals(0, $w->getHits(), 'zero hits after period was exceeded');
-        $this->assertEquals(0, $w->getTimeToWait(), 'zero time to wait after period was exceeded');
+        $w->wait($period + 1, "test_No_Hits_Zero_Wait_After_End_Of_Period"); //we exceed the limiter's period
+
+        $this->test_Zero_Hits_Zero_TimeToWait();
     }
 
-    public function testTimeToWait() {
+    public function test_Can_Increment_After_End_Of_Period() {
+        $this->test_No_Hits_Zero_Wait_After_End_Of_Period();
+        $this->test_Inc_Increments_Hits_By_One();
+    }
+
+    public function test_TimeToWait_Half_A_Period() {
         $w = $this->getLimiterWrapper();
         $rate = $w->getLimiter()->getRate();
         $period = $w->getLimiter()->getPeriod();
 
         $halfPeriod = ceil($period / 2);
-        $this->makeEquallyDistributedCalls($rate, $halfPeriod, $w, "testTimeToWait");
+        $this->makeEquallyDistributedCalls($rate, $halfPeriod, $w, "test_TimeToWait_Half_A_Period");
 
         $this->assertGreaterThan(0, $w->getTimeToWait(), 'has to wait for a positive-amount of time within an exhausted period');
         $this->assertLessThanOrEqual($halfPeriod, $w->getTimeToWait(), 'has to wait for less than a half of the period within an exhausted period');
     }
 
 
-    public function testTimeToWaitBurst() {
+    public function test_TimeToWait_Almost_A_Period_If_Burst() {
         $w = $this->getLimiterWrapper();
         $rate = $w->getLimiter()->getRate();
         $period = $w->getLimiter()->getPeriod();
 
-        $this->makeEquallyDistributedCalls($rate, 0, $w, "testTimeToWaitBurst");
+        $this->makeEquallyDistributedCalls($rate, 0, $w, "test_TimeToWait_Almost_A_Period_If_Burst");
 
         $this->assertGreaterThan(0, $w->getTimeToWait(), 'has to wait almost all the time within an exhausted period');
         $this->assertLessThanOrEqual($period - 1, $w->getTimeToWait(), 'has to wait for less than whole period within an exhausted period');
     }
 
-    public function testTooManyRequestsInOnePeriodDoNotAffectHitRate() {
+    public function test_Too_Many_Requests_In_One_Period_Do_NotAffectNumber_Of_Hits() {
         $w = $this->getLimiterWrapper();
         $rate = $w->getLimiter()->getRate();
         $period = $w->getLimiter()->getPeriod();
 
-        $successCalls = $this->makeEquallyDistributedCalls($rate * 3, $period - 3, $w, "testTooManyRequestsInOnePeriodDoNotAffectHitRate");
+        $successCalls = $this->makeEquallyDistributedCalls($rate * 3, $period - 3, $w, "Too_Many_Requests_In_One_Period_Do_NotAffectNumber_Of_Hits");
 
-        $this->assertEquals($rate, $w->getHits(), 'allowed <rate> hits within one period');
+        $this->ensureMaximumHitsIsMade();
         $this->assertEquals($rate, $successCalls, 'allowed <rate> successful requests within one period');
     }
 
-    public function testReadyInTheNextPeriod() {
+
+    public function test_Ready_In_The_Next_Period() {
         $w = $this->getLimiterWrapper();
         $rate = $w->getLimiter()->getRate();
         $period = $w->getLimiter()->getPeriod();
 
         $successCalls = $this->makeEquallyDistributedCalls($rate, $period - 3, $w, "testReadyInTheNextPeriod");
 
-        $this->assertEquals($rate, $w->getHits(), 'allowed <rate> hits within one period');
+        $this->ensureMaximumHitsIsMade();
         $this->assertEquals($rate, $successCalls, 'allowed <rate> successful requests within one period');
-
 
         $w->wait(3, "testConsumeNextPeriod");
 
-        $this->assertEquals(0, $w->getHits(), 'was reset in the next period');
-        $this->assertEquals(0, $w->getTimeToWait(), 'was reset in the next period');
+        $this->test_Zero_Hits_Zero_TimeToWait();
 
         $successCallsInNextPeriod = $this->makeEquallyDistributedCalls($rate, $period - 3, $w, "testReadyInTheNextPeriod");
 
-        $this->assertEquals($rate, $w->getHits(), 'allowed <rate> hits within the next period');
+        $this->ensureMaximumHitsIsMade();
         $this->assertEquals($rate, $successCallsInNextPeriod, 'allowed <rate> successful requests within the next period');
     }
 
+
+    public function test_No_Hits_No_TimeToWait_If_Timestamp_Before_StartTime() {
+        $w = $this->getLimiterWrapper();
+        $rate = $w->getLimiter()->getRate();
+        $period = $w->getLimiter()->getPeriod();
+
+        $halfPeriod = floor($period / 2);
+        $this->makeEquallyDistributedCalls($rate, $halfPeriod, $w, "test_No_Hits_No_TimeToWait_If_Timestamp_Before_StartTime");
+
+        $this->ensureMaximumHitsIsMade();
+
+        $timeBefore = $w->getTime() - $period - 1;
+        $this->assertEquals(0, $w->getLimiter()->getHits($timeBefore), "if timestamp before start time, reset the limiter to have 0 hits");
+        $this->assertEquals(0, $w->getLimiter()->getTimeToWait($timeBefore), "if timestamp before start time, reset the limiter to have 0 timeToWait");
+    }
 //------------------------------------------------------------------------------
 
 
@@ -251,21 +285,19 @@ class LimiterTimeWrapper {
      *
      * @return self
      */
-    public function __construct(RateLimiterInterface $limiter, $useRealTimeFlag, $startTime = 0) {
+    public function __construct(RateLimiterInterface $limiter, $useRealTimeFlag, $startTime = 3600*24) {
         $this->limiter = $limiter;
         $this->realTimeFlag = $useRealTimeFlag;
-        if ($this->realTimeFlag == true) {
+        if ($this->realTimeFlag) {
             $startTime = time();
         }
         $this->reset($startTime);
     }
 
     public function reset($startTime = 0) {
-        if ($this->realTimeFlag == true) {
-            //echo (":LimiterTimeWrapper reset startTime [$startTime]:");
+        if ($this->realTimeFlag) {
             $this->getLimiter()->reset();
         } else {
-            //echo (":LimiterTimeWrapper reset preset startTime [$startTime]:");
             $this->getLimiter()->reset($startTime);
         }
         $this->time = $startTime;
