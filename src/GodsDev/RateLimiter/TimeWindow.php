84a2f2961b583ae9c023600459e3ed9a7ad94757
tomas <tomas.kraus@icord.cz>
2017-04-25 20:56:08 +0200

    TimeWindow: proper documentation

--- src/GodsDev/RateLimiter/TimeWindow.php
+++ src/GodsDev/RateLimiter/TimeWindow.php
@@ -3,20 +3,44 @@
 namespace GodsDev\RateLimiter;
 
 /*
- * Represents a time interval (running or passed).
- * Consists of:
+ * Represents a concrete time interval (active, future or passed).
+ * A TimeWindow object is determined by its "start time" and its "period"
+ * 
+ * TODO rename active to current?
+ *
+ * fig1:
+ * <code>
+ *   
+ *                         timestamp
+ *                             |
+ *   +---------------+---------------+---------------+     +---------------+
+ *   |  TimeWindow1  |  TimeWindow2  |  TimeWindow3  |     |  TimeWindowN  |
+ *   +---------------+---------------+---------------+ ... +---------------+
+ *   |               |               |
+ *   |   period      |           endTime2, startTime3
+ *   |               |
+ *  startTime1   endTime1,startTime2
+ *   
+ * </code>
+ *   
+ * Given an TimeWindow instance TimeWindow2 from a fig1: 
  * <ul>
- *   <li>start time
- *   <li>period
+ *   <li> A "Next TimeWindow" to TimeWindow2 is TimeWindow3
+ *   <li> A startTime of TimeWindow2 has a value of startTime2, the same as the value of endTime1
+ *   <li> TimeWindow1 endtime can be computed, has value of startTime1 + period
+ * </ul>
+ * 
+ * for a timestamp given in a fig1:
+ * <ul>
+ *    <li> "ACTIVE TimeWindow" is TimeWindow2
+ *    <li> "ELAPSED Timewindow" is TimeWindow1 and all TimeWindows before
+ *    <li> "FUTURE Timewindow" is TimeWindow3 and all TimeWindows after
  * </ul>
  *
  * @author Tomáš
  */
 class TimeWindow {
 
-//    const ELAPSED = 0;
-//    const ACTIVE = 1;
-//    const FUTURE = 2;
 
     private $startTime;
     private $period;
@@ -25,68 +49,73 @@ class TimeWindow {
     /**
      * creates a new instance
      *
-     * @param integer $startTime time stamp like value (in time-units)
-     * @param integer $period duration of a window (in time-units)
+     * @param int $startTime a timestamp of a start time of this TimeWindow
+     * @param int $period duration of this TimeWindow
      */
     public function __construct($startTime, $period) {
         $this->startTime = $startTime;
         $this->period = $period;
     }
 
+    /**
+     * @return int a timestamp of start time of this TimeWindow
+     */
     public function getStartTime() {
         return $this->startTime;
     }
 
+    /**
+     * @return int a period of this TimeWindow
+     */
+     */
     public function getPeriod() {
         return $this->period;
     }
 
     /**
-     *
-     *
-     * @return integer a time stamp of immediately following next TimeWindow
+     * @return int a timestamp of start time of the next TimeWindow
      */
     public function getEndTime() {
         return $this->getStartTime() + $this->getPeriod();
     }
 
     /**
-     * Tells whether is elapsed against the $timeStamp given.
+     * Tells whether is elapsed against the $timestamp given.
      *
-     * @param integer $timeStamp a reference time stamp (in time-units)
-     * @return boolean true if, false otherwise
+     * @param int $timestamp a reference time stamp
+     * @return boolean true if a timestamp given is at or after the end-time of this TimeWindow, false otherwise
      */
-    public function isElapsed($timeStamp) {
-        return ($timeStamp >= $this->getEndTime());
+    public function isElapsed($timestamp) {
+        return ($timestamp >= $this->getEndTime());
     }
 
     /**
-     * Tells whether is in the future against the $timeStamp given.
+     * Tells whether is in the future against the $timestamp given.
      *
-     * @param integer $timeStamp a reference time stamp (in time-units)
-     * @return boolean true if, false otherwise
+     * @param int $timestamp a reference timestamp
+     * @return boolean true if a timestamp given is before start-time of this TimeWindow, false otherwise
      */
-    public function isFuture($timeStamp) {
-        return ($timeStamp < $this->getStartTime());
+    public function isFuture($timestamp) {
+        return ($timestamp < $this->getStartTime());
     }
 
     /**
      *
-     * Tells whether is within the $timeStamp given.
+     * Tells whether a $timestamp given falls into it.
      *
-     * @param integer $timeStamp a reference time stamp (in time-units)
-     * @return boolean true if, false otherwise
+     * @param int $timestamp a reference time stamp
+     * @return boolean true if a timestamp given is greater or equals to startTime and less than endTime of this TimeWindow, false otherwise
      */
-    public function isActive($timeStamp) {
-        return (!$this->isElapsed($timeStamp) && (!$this->isFuture($timeStamp)));
+    public function isActive($timestamp) {
+        return (!$this->isElapsed($timestamp) && (!$this->isFuture($timestamp)));
     }
 
 
     /**
-     * Time to wait until the next window is active, based on timestamp
+     * Time to wait until the next window is active, against the $timestamp given.
      *
-     * @param integer $timestamp a reference time stamp (in time-units)
-     * @return integer >= 0 for active or future window. < 0 for elapsed window.
+     * @param int $timestamp a reference time stamp
+     * @return int >= 0 for active or future window. < 0 for elapsed window.
      *
      * This expression is true for a window $w and timestamp $ts:
      *   $tw->getEndTime($ts) == $ts + $tw->getTimeToNext($ts)
